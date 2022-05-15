9966b1a40e768150a7bafe89a2856b77f9ed6039
Alessandro Sousa <92834495+AlessandroSousaa@users.noreply.github.com>
2022-05-15 18:18:37 -0300

    .

--- logado.js
+++ logado.js
@@ -1,11 +1,13 @@
-import React, { useState, useEffect} from 'react';
-import { StyleSheet ,Text, Button, View,FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { Text, Button, View } from 'react-native';
 import firebase from './firebaseconection';
 import { TextInput } from 'react-native-gesture-handler';
 import login from './Login';
 import { createStackNavigator } from '@react-navigation/stack';
+import { database } from 'firebase';
 
-function logado({navigation}){
+
+function logado({navigation}) {
   if (firebase.auth().currentUser !== null){
 
   }else{
@@ -14,8 +16,7 @@ function logado({navigation}){
 
     const [nome, setNome] = useState('')
     const [desc, setDesc] = useState('')
-    const [data, setData] = useState('')
-    //const [user_id, setUserId] = useState('')
+
     const onChangeNome = (txtNome) => {
         setNome(txtNome)
     }
@@ -23,22 +24,13 @@ function logado({navigation}){
         setDesc(txtDescription)
     }
     const InsertProduto = () => {
-        firebase.firestore().collection('produtos').add({nome:nome, desc:desc,});
+        firebase.collection('produtos').add({nome:nome, desc:desc});
     }
-    const Show = () => {
-        firebase.firestore().collection("produtos");
-        produtos.get().then((snapshot) => { snapshot.docs.forEach(doc => {
-             
-        })})
-    }
-    
- 
-      
-    
+
     return(
         
         <View>
-        <Text>Cadastrar Produtos</Text>
+        <Text>Cadastrar Alunos</Text>
         <TextInput
         style={{backgroundColor:'white',fontSize:16,borderRadius:15,textAlign:'center', borderWidth:5, borderColor:'red', width:300}} 
         value={nome}
@@ -52,19 +44,8 @@ function logado({navigation}){
         >
         </TextInput>
         <Button
-            buttonStyle={style.button}
             title="Cadastrar"
             onPress={InsertProduto}
-        />
-        
-        <FlatList
-          data={data}
-          renderItem={({item}) => (
-            <View style={{ marginBottom:5,borderRadius:15,backgroundColor:'black',borderWidth:4,borderColor:'green',height: 50, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-              <Text style={{color:'white', fontSize:15}}>Nome: {item.nome}</Text>
-              <Text style={{color:'white', fontSize:15}}>Descrição: {item.desc}</Text>
-            </View>
-          )}
         />
         </View>
     );
